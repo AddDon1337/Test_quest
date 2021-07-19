b7d65c52c9338a42807356509c96df9a3ca8cd99
adddon <dmitriy.lekov@mail.ru>
2021-07-19 20:58:25 +0700

    serializers3_commit

--- test_api/urls.py
+++ test_api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('test_api/', views.deal_list),
-    path('test_api/<int:pk>/', views.deal_detail)
+    path('test_api/', views.DealList.as_view()),
+    path('test_api/<int:pk>/', views.DealDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- test_api/views.py
+++ test_api/views.py
@@ -1,52 +1,17 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+
 from test_api.models import Deal
 from test_api.serializers import DealSerializer
+from rest_framework import generics
 
 
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def deal_list(request, format=None):
-    """
-    List all code deals, or create a new deal.
-    """
-    if request.method == 'GET':
-        deal = Deal.objects.all()
-        serializer = DealSerializer(deal, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = DealSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class DealList(generics.ListCreateAPIView):
+    queryset = Deal.objects.all()
+    serializer_class = DealSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def deal_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a code deal.
-    """
-    try:
-        deal = Deal.objects.get(pk=pk)
-    except Deal.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = DealSerializer(deal)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = DealSerializer(deal, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        deal.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class DealDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Deal.objects.all()
+    serializer_class = DealSerializer
